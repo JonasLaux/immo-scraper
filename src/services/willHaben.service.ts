edb15b31769b9a8068d54b81a8db0cabf85d8118
Jonas Laux <jonas@anyline.io>
2023-05-14 22:34:23 +0200

    (feat): add discord message publishing

--- src/services/willHaben.service.ts
+++ src/services/willHaben.service.ts
@@ -1,32 +1,84 @@
 import { chromium } from 'playwright';
+import { FlatElement } from '../types';
+const dayjs = require('dayjs');
 
-type FlatElement = {
-  description: string;
-  id: string;
+type AdvertImage = {
+  mainImageUrl: string;
 };
 
-export class WillHabenService {
-  private url = `https://www.willhaben.at/iad/immobilien/mietwohnungen/mietwohnung-angebote?areaId=117224&PRICE_TO=2500&ESTATE_SIZE/LIVING_AREA_FROM=90`;
-  public async doScrape() {
+type ContextLinks = {
+  id: string;
+  uri: string;
+};
+type Attribute = {
+  name: string;
+  values: string[];
+};
+
+type TeaserAttributes = {
+  postfix: string;
+  value: string;
+};
+export default class WillHabenService {
+  private url =
+    'https://www.willhaben.at/iad/immobilien/mietwohnungen/mietwohnung-angebote?areaId=117224&PRICE_TO=2500&ESTATE_SIZE/LIVING_AREA_FROM=90';
+  public async getFlats(): Promise<FlatElement[]> {
     const browser = await chromium.launch({
       headless: true,
     });
 
     const page = await browser.newPage();
-    await page.goto(this.url, {});
+    await page.goto(this.url);
 
     try {
       const dataRaw = await page.locator('#__NEXT_DATA__').textContent();
       if (dataRaw) {
+        const flats: Array<FlatElement> = [];
         const data = JSON.parse(dataRaw);
         data.props?.pageProps?.searchResult?.advertSummaryList?.advertSummary?.forEach(
-          (element: FlatElement) => {
-            console.log(element.description);
+          (element: any) => {
+            // Get that date
+            const publishedAtRaw = JSON.parse(
+              element.attributes.attribute.find(
+                (attr: Attribute) => attr.name === 'PUBLISHED'
+              ).values[0]
+            );
+
+            const images = element.advertImageList.advertImage.map(
+              (image: AdvertImage) => image.mainImageUrl
+            );
+            const publishedAt = dayjs(publishedAtRaw).format('DD.MM.YYYY');
+
+            const newFlat: FlatElement = {
+              description: element.description,
+              link: element.contextLinkList.contextLink.find(
+                (links: ContextLinks) => links.id === 'iadShareLink'
+              ).uri,
+              location: element.attributes.attribute.find(
+                (attr: Attribute) => attr.name === 'LOCATION'
+              ).values[0],
+              price: element.attributes.attribute.find(
+                (attr: Attribute) => attr.name === 'PRICE_FOR_DISPLAY'
+              ).values[0],
+              squareMeters: element.teaserAttributes.find(
+                (attr: TeaserAttributes) => attr.postfix === 'm²'
+              ).value,
+              rooms: element.teaserAttributes.find(
+                (attr: TeaserAttributes) => attr.postfix === 'Zimmer'
+              ).value,
+              publishedAt,
+              images,
+            };
+            flats.push(newFlat);
           }
         );
+        return Promise.resolve(flats);
+      } else {
+        return Promise.reject('No data found!');
       }
     } catch (err) {
       console.error(err);
+      return Promise.reject(err);
     } finally {
       await browser.close();
     }
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type FlatElement = {
+  description: string;
+  link: string;
+  location: string;
+  price: string;
+  rooms: number;
+  squareMeters: number;
+  publishedAt: string;
+  images?: string[];
+};
