ab2648dbc75ccd5cbbf8d30f2358a078a7bd038a
Jonas Laux <jonas.laux@anyline.com>
2023-08-12 11:59:53 +0200

    (fix): Add Entrypoint of Function

--- src/functions/scrape/index.ts
+++ src/functions/scrape/index.ts
@@ -8,7 +8,37 @@ import willHabenConfig from './willhaben.config';
 const willHabenService = new WillHabenService();
 
 http('scrape', async (req: Request, res: Response) => {
-  // await willHabenService.getFlats();
+  if (
+    !process.env.DISCORD_TOKEN &&
+    typeof process.env.DISCORD_TOKEN !== 'string'
+  ) {
+    throw new Error('DISCORD_TOKEN env variable is not set');
+  }
+  if (
+    !process.env.DISCORD_CHANNEL_ID &&
+    typeof process.env.DISCORD_CHANNEL_ID !== 'string'
+  ) {
+    throw new Error('DISCORD_CHANNEL_ID env variable is not set');
+  }
+  const discordService = new DiscordService(
+    process.env.DISCORD_TOKEN,
+    process.env.DISCORD_CHANNEL_ID
+  );
+
+  let flats = await willHabenService.getFlats(willHabenConfig);
+  flats = flats.slice(0, 3);
+  await discordService.connect();
+  await Promise.all(
+    flats.map(async flat => {
+      const isMessagePosted = await discordService.isFlatPosted(flats[0].id);
+      if (!isMessagePosted) {
+        return discordService.publishMessage(
+          discordService.constructEmbed(flat)
+        );
+      }
+    })
+  );
+  discordService.disconnect();
   res.send('OK!');
 });
 
@@ -47,4 +77,4 @@ const main = async () => {
   discordService.disconnect();
 };
 
-main();
+// main();
